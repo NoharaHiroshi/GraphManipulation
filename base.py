a6c82a7906e2582e4937861f4712a2130aca3cc4
Lands <380788433@qq.com>
2017-06-06 18:22:19 +0800

    测试图片操作

--- base.py
+++ base.py
@@ -5,14 +5,19 @@ import contextlib
 from PIL import Image
 
 
+class NewImage:
+    def __init__(self, file_name):
+        self.base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.img_dir = os.path.join(self.base_dir, u'GraphManipulation\Images')
+        self.file_dir = os.path.join(self.img_dir, file_name)
+        self.im = Image.open(self.file_dir)
+
+
 @contextlib.contextmanager
 def open_image(file_name):
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    img_dir = os.path.join(base_dir, u'GraphManipulation\Images')
-    file_dir = os.path.join(img_dir, file_name)
-    im = Image.open(file_dir)
+    new_im = NewImage(file_name)
     try:
-        yield im
+        yield new_im
     except Exception as e:
         print e
     finally:
--- graph_manipulation.py
+++ graph_manipulation.py
@@ -1,13 +1,15 @@
 # coding=utf-8
 
+import os
 from base import open_image
 
 
 def base_image(img):
     try:
         with open_image(img) as image:
-            image.convert("1")
-            print image.mode
+            new_img = image.im.convert("1")
+            new_dir = os.path.join(image.img_dir, 'new_img.jpg')
+            new_img.save(new_dir)
     except Exception as e:
         print e
 
