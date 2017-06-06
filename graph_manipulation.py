361f9cd674fb3cfbe50e72caff6ecb51be149ea2
Lands <380788433@qq.com>
2017-06-06 22:44:25 +0800

    数组与图形的转换

--- graph_manipulation.py
+++ graph_manipulation.py
@@ -1,17 +1,24 @@
 # coding=utf-8
 
 import os
+import numpy as np
+import matplotlib.image as mpimg
 from base import open_image
+from PIL import Image
 
 
 def base_image(img):
     try:
         with open_image(img) as image:
-            new_img = image.im.convert("1")
-            new_dir = os.path.join(image.img_dir, 'new_img.jpg')
-            new_img.save(new_dir)
+            new_img = image.im
+            img_arr = np.array(new_img)
+            con_img = Image.fromarray(img_arr)
+            con_img.show()
     except Exception as e:
         print e
 
 if __name__ == '__main__':
-    base_image(u'img.jpg')
+    img = mpimg.imread(u'Images/cat_1.jpg')
+    img *= 255
+    im = Image.fromarray(np.uint8(img))
+    im.show()
