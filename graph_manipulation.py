66c8d58d64e840c9ce9216dd5ba0b959fe5f25fe
Lands <380788433@qq.com>
2017-06-08 22:57:06 +0800

    图片与字符的转换

--- graph_manipulation.py
+++ graph_manipulation.py
@@ -6,19 +6,25 @@ import matplotlib.image as mpimg
 from base import open_image
 from PIL import Image
 
+convert_char = [u'█', u'▉', u'▊', u'▌', u'▍', u'▎', u'▏']
+
 
 def base_image(img):
     try:
         with open_image(img) as image:
-            new_img = image.im
-            img_arr = np.array(new_img)
-            con_img = Image.fromarray(img_arr)
-            con_img.show()
+            new_img = image.im.convert('1').resize((128, 128))
+            width, height = new_img.size
+            # 逐行扫描，高度为0,1,2,3。。。
+            for h in range(height):
+                row = list()
+                for w in range(width):
+                    pixel = new_img.getpixel((w, h))
+                    con = len(convert_char) - 1
+                    flag = pixel // (255 // con)
+                    row.append(u'%s(%s)' % (convert_char[flag], flag))
+                print ' '.join(row)
     except Exception as e:
         print e
 
 if __name__ == '__main__':
-    img = mpimg.imread(u'Images/cat_1.jpg')
-    img *= 255
-    im = Image.fromarray(np.uint8(img))
-    im.show()
+    base_image(u'img.png')
