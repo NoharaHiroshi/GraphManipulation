435b7eec899012577b8d4eec713dc72261f648a8
Lands <380788433@qq.com>
2017-06-09 18:47:17 +0800

    压缩图片

--- graph_manipulation_1.py
+++ graph_manipulation_1.py
@@ -32,5 +32,38 @@ def transition_picture_format(img, img_type):
     except Exception as e:
         print e
 
+
+# Create Thumbnail
+def create_thumbnail(img, rate=None, resize=None):
+    status = u'success'
+    try:
+        with open_image(img) as image:
+            new_img = image.im
+            new_size = new_img.size
+            width, height = new_size
+            if resize:
+                if isinstance(resize, tuple):
+                    new_size = resize
+                else:
+                    status = u'fail'
+                    return status
+            elif rate:
+                if isinstance(rate, (int, float)):
+                    _width = int(width * rate)
+                    _height = int(height * rate)
+                    new_size = _width, _height
+                else:
+                    status = u'fail'
+                    return status
+            else:
+                status = u'fail'
+                return status
+            new_img.thumbnail(new_size)
+            new_img.show()
+        return status
+    except Exception as e:
+        print e
+
+
 if __name__ == '__main__':
-    transition_picture_format(u'cat.jpg', u'png')
+    create_thumbnail(u'img.png', resize=(200, 200))
